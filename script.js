30a49a5e4e976ab257d270858121dbfc7d46bc0d
Suraj-kp-03 <surajkp3685@gmail.com>
2025-05-31 13:10:17 +0530

    script.js
    Added the version of js code without state management and localstorage

--- script.js
+++ script.js
@@ -115,3 +115,84 @@ function taskModify(event) {
 document.addEventListener('DOMContentLoaded', loadTasks);
 form.addEventListener('submit', addTask);
 taskList.addEventListener('click', taskModify);
+
+
+
+
+
+
+
+
+
+/*
+BEFORE STATE MANAGEMENT
+// Select the necessary css elements to perform thr task - Only for making the task to be deigned accurately
+const form = document.querySelector('.input-field'); //New task input field
+const input = document.querySelector('input');//input refers to the input tag whic is used to get the input
+const taskList = document.querySelector('.task-list');// tasks after added
+
+
+// Function to add a new task
+function addTask(event) {
+
+    // to prevent the page from reloading on form submission
+    event.preventDefault();
+
+    // get the value of the input field, input refers the input attribute tag
+    const inputValue = input.value;
+    console.log(inputValue);//checing whether the input is received correctly
+
+    // create the necessary elements for the new task
+    const listItem = document.createElement('li');
+    const taskDiv = document.createElement('div');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    const taskName = document.createElement('span');
+    taskName.innerText = inputValue;
+    const deleteButton = document.createElement('button');
+
+    // add the necessary classes and icons to the buttons
+    taskDiv.classList.add('task-info');
+    deleteButton.classList.add('delete');
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash" style="color: #1e2f67;"></i>    
+`
+
+    // append the necessary elements to the li and ul to display in webpage
+    taskList.appendChild(listItem);
+    listItem.appendChild(taskDiv);
+    listItem.appendChild(deleteButton);
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(taskName);
+    // clear the input field after adding the new task
+    input.value = '';
+}
+
+// Function to modify a task
+function taskModify(event) {
+    const clickedEle = event.target;// storing the element clicked 
+    console.log(clickedEle);
+    
+    // check if the delete button was clicked
+    if (clickedEle.classList[1] === 'fa-trash') {
+        const task = clickedEle.parentElement.parentElement;
+        task.remove();
+    }
+
+    // check if the text was clicked
+    if (clickedEle.tagName.toLowerCase() === 'span'){
+        const task = clickedEle.innerHTML;
+        clickedEle.innerHTML = `<input class="edit-input" type="text" value="${task}"/>`;
+
+        const editInput = clickedEle.querySelector('input');
+        editInput.addEventListener('blur', ()=>{
+            const newValue = editInput.value;
+            clickedEle.innerHTML = newValue;
+        });
+    }
+
+// Event listeners to add a new task and edit, delete a task
+form.addEventListener('submit', addTask);
+// Event listeners to add a new task and edit, delete a task
+taskList.addEventListener('click', taskModify)
+*/
+
